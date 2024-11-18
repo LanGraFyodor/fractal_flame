5f37dd945fd404ba35cfd1f42176b9a64cd072c4
Фёдор Бурлака <burlakaf@yandex.ru>
2024-11-18 04:05:42 +0300

    Точки

---
+++ src/main/java/backend/academy/Point.java
@@ -0,0 +1,13 @@
+package backend.academy;
+
+public class Point {
+    public double x;
+    public double y;
+    public double color;
+
+    public Point(double x, double y, double color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+}
